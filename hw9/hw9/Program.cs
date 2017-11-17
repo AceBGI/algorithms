a17c3437ca12e410e54937d2cdbb3db8148c64c3
u0584599 <u0584599@eae-d-074>
2017-11-16 20:15:56 -0700

    starting new project

---
+++ hw9/hw9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> penalty = new HashSet<int>();
+            List<int> dist = new List<int>();
+
+            string firstLine = Console.ReadLine();
+            int N;
+            Int32.TryParse(firstLine, out N);
+            N++;
+
+            for (int i = 0; i < N; i++)
+            {
+                string line = Console.ReadLine();
+                int d;
+                Int32.TryParse(line, out d);
+
+                dist.Add(d);
+            }
+
+            // penalty(i) = 400 - (dist(k) - dist(i)) ^ 2 + penalty(k)
+        }
+    }
+}
