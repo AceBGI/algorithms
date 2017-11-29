28c67cc49591826920ca5b1acdf6fca2435bc30b
AceBGI <ace-bgi@hotmail.com>
2017-11-29 10:07:49 -0700

    working on spider man

--- spiderMan/spiderMan/Program.cs
+++ spiderMan/spiderMan/Program.cs
@@ -10,6 +10,10 @@ namespace spiderMan
     {
         static void Main(string[] args)
         {
+            // var
+            int up = 0;
+            int down = 0;
+
             string firstLine = Console.ReadLine();
             int N;
             Int32.TryParse(firstLine, out N);
@@ -17,21 +21,50 @@ namespace spiderMan
             for (int i = 0; i < N; i++)
             {
                 string lineNum = Console.ReadLine();
-                int Num;
-                Int32.TryParse(lineNum, out Num);
+                int M;
+                Int32.TryParse(lineNum, out M);
+
+                int[] distances = new int[M+1];
+                int[,] a = new int[41,1001];
+                int[,] direction = new int[41, 1001];
+                Array.Clear(direction, 0, direction.Length);
+
+                string line = Console.ReadLine();
+                string[] lineD = line.Split(new[] { '\t', ' ' });
+                distances = Array.ConvertAll(lineD, int.Parse);
+
+                
+
+
+                workOut(distances, a, direction, M, up, down);
 
 
             }
-            string line = Console.ReadLine();
-            string[] lineD = line.Split(new[] { '\t', ' ' });
-            int[] distances = Array.ConvertAll(lineD, int.Parse);
-
-            workOut(distances);
         }
 
-        private static void workOut(int[] distances)
+        private static void workOut(int[] distances, int[,] a, int[,] direction, int M, int up, int down)
         {
-            throw new NotImplementedException();
+            a[0, 0] = 0;
+            for (int i = 0; i <= M; i++)
+            {
+                for (int j = 0; j <= 1000 - distances[i]; j++)
+                {
+                    up = j + distances[i];
+                    down = j - distances[i];
+
+                    // you can not go underground, down != -number
+                    if(down >= 0)
+                    {
+
+                    }
+                    else if (a[i-1,up] != -1)
+                    {
+                        a[i, j] = a[i - 1, up];
+                        direction[i, j] = -1;
+                        //https://github.com/meysamaghighi/Kattis/blob/master/Spiderman's%20workout/a.cpp
+                    }
+                }
+            }
         }
     }
 }
